9749a9ad0d1e1c782c82c8e4be20334ae8a0d589
EthanBarrett <145779498+EthanBarrett@users.noreply.github.com>
2024-11-29 16:45:19 +0000

    image work

--- webGame/nodejsProject/myGame/app.js
+++ webGame/nodejsProject/myGame/app.js
@@ -13,7 +13,7 @@ var con = mysql.createConnection({
 });
 
 //set up static folder
-app.use(express.static('myGame/static'));
+app.use(express.static('myGame'));
 
 con.connect(function(err) {
   if (err) throw err;
